d9d4a226c5552d65e2048d40e717e1673e3ddd87
wurban <wurban@somex.ie>
2017-12-18 13:30:13 +0000

    Add project files.

---
+++ ToDoAppWindowsClient/View/Main.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ToDoAppWindowsClient.View
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        private readonly AmazonCognitoIdentityProviderClient _client = new AmazonCognitoIdentityProviderClient();
+        private readonly string _clientId = ConfigurationManager.AppSettings["CLIENT_ID"];
+        private readonly string _poolId = ConfigurationManager.AppSettings["USERPOOL_ID"];
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp signUpDlg = new SignUp(_client);
+            signUpDlg.ShowDialog();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool loggedIn = await CheckPasswordAsync(UserNameTextBox.Text, PasswordTextBox.Password);
+        }
+
+        private async Task<bool> CheckPasswordAsync(string userName, string password)
+        {
+            try
+            {
+                var authReq = new AdminInitiateAuthRequest()
+                {
+                    UserPoolId = _poolId,
+                    ClientId = _clientId,
+                    AuthFlow = AuthFlowType.ADMIN_NO_SRP_AUTH
+                };
+                authReq.AuthParameters.Add("USERNAME", userName);
+                authReq.AuthParameters.Add("PASSWORD", password);
+
+                AdminInitiateAuthResponse authResp = await _client.AdminInitiateAuthAsync(authReq);
+
+                authResp.AuthenticationResult.
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ToDoAppWindowsClient/View/SignUp.xaml.cs
@@ -0,0 +1,87 @@
+﻿using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoAppWindowsClient.View
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        private readonly string _clientId = ConfigurationManager.AppSettings["CLIENT_ID"];
+        private readonly string _poolId = ConfigurationManager.AppSettings["USERPOOL_ID"];
+        private AmazonCognitoIdentityProviderClient _client;
+
+        public SignUp(AmazonCognitoIdentityProviderClient client)
+        {
+            InitializeComponent();
+
+            _client = client;
+        }
+
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private async void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SignUpRequest signUpRequest = new SignUpRequest()
+                {
+                    ClientId = _clientId,
+                    Password = PasswordTextBox.Password,
+                    Username = UserNameTextBox.Text
+                };
+                AttributeType emailAttribute = new AttributeType()
+                {
+                    Name = "email",
+                    Value = EmailTextBox.Text
+                };
+                signUpRequest.UserAttributes.Add(emailAttribute);
+
+                var signUpResult = await _client.SignUpAsync(signUpRequest);
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                MessageBox.Show(message, "Sign Up Error");
+            }
+        }
+
+        private async void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Amazon.CognitoIdentityProvider.Model.ConfirmSignUpRequest confirmRequest = new ConfirmSignUpRequest()
+                {
+                    Username = UserNameTextBox.Text,
+                    ClientId = _clientId,
+                    ConfirmationCode = ConfirmationTextBox.Text
+                };
+
+                var confirmResult = await _client.ConfirmSignUpAsync(confirmRequest);
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                MessageBox.Show(message, "Sign Up Error");
+            }
+        }
+    }
+}
